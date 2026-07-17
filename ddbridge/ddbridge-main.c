a22c57e2756d5b33fe7c25cd4f17cf3f8e434e93
internal <>
2026-07-17 10:56:31 +0200

    add entry for M4 V2

--- ddbridge/ddbridge-main.c
+++ ddbridge/ddbridge-main.c
@@ -419,6 +419,7 @@ static const struct pci_device_id ddb_id_table[] __devinitconst = {
 	DDB_DEVICE_ANY(0x0013),
 	DDB_DEVICE_ANY(0x0014),
 	DDB_DEVICE_ANY(0x0020),
+	DDB_DEVICE_ANY(0x0021),
 	DDB_DEVICE_ANY(0x0022),
 	DDB_DEVICE_ANY(0x0023),
 	DDB_DEVICE_ANY(0x0024),
