240f32ae99a25ba78af62b82a4f1b0e58c87c1d7
internal <>
2026-08-26 23:44:06 +0200

    remove leftover printks

--- ddbridge/ddbridge-sx8.c
+++ ddbridge/ddbridge-sx8.c
@@ -376,12 +376,10 @@ unlock:
 	ddb_mci_tsconfig(&state->mci, ts_config);
 	if (p->stream_id != NO_STREAM_ID_FILTER && !(p->stream_id & 0xf0000000))
 		flags |= 0x80;
-	//printk("bw %u\n", p->bandwidth_hz);
 	if (p->bandwidth_hz && (p->bandwidth_hz < 20000)) {
 		flags |= 0x40;
 		/* +/- range, so multiply bandwidth_hz (actually in kHz) by 500 */
 		cmd.dvbs2_search.frequency_range = p->bandwidth_hz * 500;
-		//printk("range %u\n", cmd.dvbs2_search.frequency_range);
 	}
 	cmd.command = MCI_CMD_SEARCH_DVBS;
 	cmd.dvbs2_search.flags = flags;
@@ -472,7 +470,6 @@ unlock:
 
 static int set_lna(struct dvb_frontend *fe)
 {
-	printk("set_lna\n");
 	return 0;
 }
 
