6b4fe98903dc5c46f3e3311c864574326c67bbe1
internal <>
2026-09-14 09:36:14 +0200

    use proper enum (SEC_TONE_OFF = 1 !) and comment out until further tested

--- ddbridge/ddbridge-max.c
+++ ddbridge/ddbridge-max.c
@@ -225,7 +225,7 @@ static int max_set_input_unlocked(struct dvb_frontend *fe, int in)
 
 	if (in > 3)
 		return -EINVAL;
-	lnb_set_tone(dev, port->lnr, input->nr, 0);
+	//lnb_set_tone(dev, port->lnr, input->nr, SEC_TONE_OFF);
 
 	if (dvb->input != in) {
 		u32 bit = (1ULL << input->nr);
